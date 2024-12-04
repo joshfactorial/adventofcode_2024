515c3f8f1dd1a8159c9dcd5871c3f2e9bb937a83
Joshua Allen <joshfactorial@gmail.com>
2024-12-03 22:56:26 -0600

    My solution to 12/3

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,20 @@ fn parse_lines(lines: io::Lines<BufReader<File>>) -> u32 {
         match line {
             Ok(l) => {
                 let mut commands: Vec<&str> = Vec::new();
-                let re = Regex::new(r"mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)|don't\(\)|do\(\)").unwrap();
+                let re = Regex::new(
+                    r"mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)|don't\(\)|do\(\)"
+                ).unwrap();
                 for command in re.find_iter(&l) {
                     match &command.as_str()[0..3] {
                         "mul" => {
                             if act {
-                                let digit_seek = Regex::new(r"[0-9][0-9]?[0-9]?").unwrap();
-                                let mults: Vec<u32> = digit_seek.find_iter(command.as_str()).map(|m| m.as_str().parse::<u32>().unwrap()).collect();
+                                let digit_seek = Regex::new(
+                                    r"[0-9][0-9]?[0-9]?"
+                                ).unwrap();
+                                let mults: Vec<u32> = digit_seek
+                                    .find_iter(command.as_str())
+                                    .map(|m| m.as_str().parse::<u32>().unwrap())
+                                    .collect();
                                 running_total += mults[0] * mults[1];
                             }
                         },
@@ -46,12 +53,4 @@ pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> 
     // This creates a buffer to read lines
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
-}
-
-pub fn open_file(mut filename: &mut str, overwrite_file: bool) -> Result<File, Error> {
-    if overwrite_file && Path::new(filename).exists() {
-        File::options().create(true).write(true).open(&mut filename)
-    } else {
-        File::options().create_new(true).append(true).open(&mut filename)
-    }
 }
