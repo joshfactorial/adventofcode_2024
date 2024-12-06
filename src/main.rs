cce0e8ae4a07e257344bae4a77b20a1fc19cad19
Joshua Allen <joshfactorial@gmail.com>
2024-12-06 17:56:10 -0600

    12/5 puzzle solutions

--- src/main.rs
+++ src/main.rs
@@ -3,82 +3,108 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
-type ParsedInput = HashMap<(i16, i16), char>;
+type Rules = HashMap<String, Vec<String>>;
+type Tests = Vec<Vec<String>>;
 
 fn main() {
     let input_file = "/home/joshfactorial/code/inputs/inputs.txt.full";
     let lines = read_lines(input_file).unwrap();
-    let input = parse_lines(lines);
-    let output = part2(&input);
-    println!("Output = {output}")
+    let (rules, tests) = parse_lines(lines);
+    let (total, wrongs) = part1(&rules, &tests);
+    println!("part 2 count: {}", part2(&rules, &wrongs));
 }
 
-fn parse_lines(lines: io::Lines<BufReader<File>>) -> ParsedInput {
-    let mut output = HashMap::new();
-
-    for (y, line) in lines.enumerate() {
-        for (x, c) in line.unwrap().char_indices() {
-            output.insert((x as i16, y as i16), c);
+fn parse_lines(lines: io::Lines<BufReader<File>>) -> (Rules, Tests) {
+    let mut rules: Rules = HashMap::new();
+    let mut pages = Vec::new();
+    let mut section2 = false;
+    for line in lines {
+        if !section2 {
+            let test = line.unwrap();
+            if test == "" {
+                section2 = true
+            } else {
+                let mut split_test = test.split("|");
+                // not good code, maybe because I'm taking or granted the input is clean
+                let (low, high) = (
+                    split_test.next().unwrap_or("file format error").to_string(),
+                    split_test.next().unwrap_or("file format error").to_string()
+                );
+                rules.entry(low.clone()).or_insert(Vec::new());
+                rules.get_mut(&low).unwrap().push(high.clone());
+            }
+        } else {
+            let split_line = line
+                .unwrap()
+                .split(",")
+                .map(|m| m.to_string())
+                .collect();
+            pages.push(split_line);
         }
     }
-
-    output
+    (rules, pages)
 }
 
-fn part1(input: &ParsedInput) -> u32 {
-    let mut count = 0;
-    let neighbors: [[(i16,i16);3];8] = [
-        [( 0, -1), ( 0, -2), ( 0, -3)], // left
-        [( 0,  1), ( 0,  2), ( 0,  3)], // right
-
-        [(-1, -1), (-2, -2), (-3, -3)], // up/left
-        [(-1,  0), (-2,  0), (-3,  0)], // up
-        [(-1,  1), (-2,  2), (-3,  3)], // up/right
-
-        [( 1,  1), ( 2,  2), ( 3,  3)], // down/right
-        [( 1,  0), ( 2,  0), ( 3,  0)], // down
-        [( 1, -1), ( 2, -2), ( 3, -3)], // down/left
-    ];
-
-    for ((x, y), chr) in input {
-        if *chr != 'X' { continue }
-        for [(mx, my), (ax, ay), (sx, sy)] in neighbors {
-            if input.get(&(x + mx, y + my)) == Some(&'M') &&
-                input.get(&(x + ax, y + ay)) == Some(&'A') &&
-                input.get(&(x + sx, y + sy)) == Some(&'S') {
-                count += 1;
+fn part1(rules: &Rules, tests: &Tests) -> (u32, Tests) {
+    let mut final_score: u32 = 0;
+    let mut wrongs = Vec::new();
+    let keys_list: Vec<String> = rules.keys()
+        .map(|k| k.to_owned())
+        .collect();
+    'outer: for list in tests {
+        for i in (1..list.len()).rev() {
+            let current_page = list[i].clone();
+            // no applicable rules
+            if !keys_list.contains(&current_page) { continue };
+            let prev_pages = Vec::from(&list[..i]);
+            let curr_p_list = rules[&current_page].clone();
+            for page in prev_pages {
+                if curr_p_list.contains(&page) {
+                    // rule violation
+                    wrongs.push(list.clone());
+                    continue 'outer
+                }
             }
         }
+        // valid test
+        let midpoint = list.len()/2usize;
+        final_score += list[midpoint].clone().parse::<u32>().unwrap();
     }
-    count
+    (final_score, wrongs)
 }
 
-fn part2(input: &ParsedInput) -> u32 {
-    let mut count = 0;
-    // (1) M . M  (2) M . S (3) S . S (4)  S . M
-    //     . A .      . A .     . A .      . A .
-    //     S . S      M . S     M . M      S . M
-    //
-    let neighbors: [[(i16,i16);4];4] = [
-        [(-1, -1), (1,  -1), ( 1,  1), (-1,  1)], // M, M, S, S
-        [(-1, -1), (-1,  1), ( 1, -1), ( 1,  1)], // M, M, S, S
-        [(-1,  1), ( 1,  1), (-1, -1), ( 1, -1)], // M, M, S, S
-        [( 1,  1), ( 1, -1), (-1, -1), (-1,  1)], // M, M, S, S
-    ];
-
-    for ((x, y), chr) in input {
-        if *chr != 'A' { continue }
-        for [(mx, my), (ax, ay), (sx, sy), (tx, ty)] in neighbors {
-            if input.get(&(x + mx, y + my)) == Some(&'M') &&
-                input.get(&(x + ax, y + ay)) == Some(&'M') &&
-                input.get(&(x + sx, y + sy)) == Some(&'S') &&
-                input.get(&(x + tx, y + ty)) == Some(&'S') {
-                println!("Hit on {}, {}", x, y);
-                count += 1;
+fn part2(rules: &Rules, tests: &Tests) -> u32 {
+    // fix bad tests then count
+    let mut tally = 0;
+    let keys_list: Vec<String> = rules.keys()
+        .map(|k| k.to_owned())
+        .collect();
+    for fail in tests {
+        let mut revised = fail.clone();
+        let test_len = fail.len();
+        'outer: loop {
+            for mut i in (1..test_len).rev() {
+                let current_page = revised[i].clone();
+                if !keys_list.contains(&current_page) { continue };
+                let prev_pages = Vec::from(&revised[..i]);
+                let curr_p_list = rules[&current_page].clone();
+                for (index, page) in prev_pages.iter().enumerate() {
+                    if curr_p_list.contains(&page) {
+                        // rule violation
+                        revised[i] = page.to_owned();
+                        revised[index] = current_page.to_owned();
+                        i = test_len;
+                        continue 'outer
+                    }
+                }
             }
+            // tally midpoint
+            let midpoint = test_len/2usize;
+            tally += revised[midpoint].clone().parse::<u32>().unwrap();
+            break 'outer
         }
     }
-    count
+    tally
 }
 
 pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
