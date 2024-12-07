5b4c737c69589fa4c625783bdab85f02ea3d750f
Joshua Allen <joshfactorial@gmail.com>
2024-12-07 11:53:58 -0600

    part 1 day 6 complete

--- src/main.rs
+++ src/main.rs
@@ -2,108 +2,86 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
 
-type Rules = HashMap<String, Vec<String>>;
-type Tests = Vec<Vec<String>>;
+type Position = (usize, usize);
+
+type Obstacles = Vec<Position>;
 
 fn main() {
     let input_file = "/home/joshfactorial/code/inputs/inputs.txt.full";
     let lines = read_lines(input_file).unwrap();
-    let (rules, tests) = parse_lines(lines);
-    let (total, wrongs) = part1(&rules, &tests);
-    println!("part 2 count: {}", part2(&rules, &wrongs));
+    let (obstacles, mut guard_pos, max_pos) = parse_lines(lines);
+    let positions = part1(&obstacles, &mut guard_pos, &max_pos);
+    println!("part 1 distinct positions: {}", positions);
 }
 
-fn parse_lines(lines: io::Lines<BufReader<File>>) -> (Rules, Tests) {
-    let mut rules: Rules = HashMap::new();
-    let mut pages = Vec::new();
-    let mut section2 = false;
-    for line in lines {
-        if !section2 {
-            let test = line.unwrap();
-            if test == "" {
-                section2 = true
-            } else {
-                let mut split_test = test.split("|");
-                // not good code, maybe because I'm taking or granted the input is clean
-                let (low, high) = (
-                    split_test.next().unwrap_or("file format error").to_string(),
-                    split_test.next().unwrap_or("file format error").to_string()
-                );
-                rules.entry(low.clone()).or_insert(Vec::new());
-                rules.get_mut(&low).unwrap().push(high.clone());
+fn parse_lines(lines: io::Lines<BufReader<File>>) -> (Obstacles, Position, Position) {
+    let mut obstacles: Vec<(usize, usize)> = Vec::new();
+    let mut guard_position: (usize, usize) = (0, 0);
+    let mut max_coords: (usize, usize) = (0, 0);
+    for (row, line) in lines.enumerate() {
+        max_coords.0 += 1;
+        for (col, char) in line.unwrap().chars().enumerate() {
+            max_coords.1 += 1;
+            match char {
+                '#' => { obstacles.push((row, col)); },
+                '^' => { guard_position = (row, col); },
+                _ => { continue },
             }
+        }
+    }
+    (obstacles, guard_position, max_coords)
+}
+
+fn part1(obstacles: &Obstacles, initial_guard_pos: &Position, max_pos: &Position) -> usize {
+    //move guard
+    // add the starting pos.
+    let mut cells_entered = HashSet::new();
+    let directions: HashMap<char, (i32, i32)> = HashMap::from([
+        ('^', (-1,  0)),
+        ('>', ( 0,  1)),
+        ('v', ( 1,  0)),
+        ('<', ( 0, -1)),
+    ]);
+    let mut current_direction = '^';
+    let mut guard_pos = initial_guard_pos.clone();
+    cells_entered.insert(guard_pos.clone());
+    while guard_pos.0 > 0 &&
+        guard_pos.1 > 0 &&
+        guard_pos.0 < max_pos.0 &&
+        guard_pos.1 < max_pos.1 {
+        let current_delta = directions[&current_direction];
+        let guard_x = guard_pos.0 as i32;
+        let guard_y = guard_pos.1 as i32;
+        let mut dest = (
+            (guard_x + current_delta.0) as usize,
+            (guard_y + current_delta.1) as usize,
+        );
+        if obstacles.contains(&dest) {
+            // turn
+            current_direction = match current_direction {
+                '^' => '>',
+                '>' => 'v',
+                'v' => '<',
+                '<' => '^',
+                _ => panic!("unknown direction")
+            }
+        } else if (0..max_pos.0).contains(&dest.0) &&
+            (0..max_pos.1).contains(&dest.1) {
+            // move
+            cells_entered.insert(dest.clone());
+            guard_pos = dest.clone();
         } else {
-            let split_line = line
-                .unwrap()
-                .split(",")
-                .map(|m| m.to_string())
-                .collect();
-            pages.push(split_line);
+            // set guard pos out of bounds to end loop
+            guard_pos = dest.clone()
         }
     }
-    (rules, pages)
+    cells_entered.len()
 }
 
-fn part1(rules: &Rules, tests: &Tests) -> (u32, Tests) {
-    let mut final_score: u32 = 0;
-    let mut wrongs = Vec::new();
-    let keys_list: Vec<String> = rules.keys()
-        .map(|k| k.to_owned())
-        .collect();
-    'outer: for list in tests {
-        for i in (1..list.len()).rev() {
-            let current_page = list[i].clone();
-            // no applicable rules
-            if !keys_list.contains(&current_page) { continue };
-            let prev_pages = Vec::from(&list[..i]);
-            let curr_p_list = rules[&current_page].clone();
-            for page in prev_pages {
-                if curr_p_list.contains(&page) {
-                    // rule violation
-                    wrongs.push(list.clone());
-                    continue 'outer
-                }
-            }
-        }
-        // valid test
-        let midpoint = list.len()/2usize;
-        final_score += list[midpoint].clone().parse::<u32>().unwrap();
-    }
-    (final_score, wrongs)
-}
-
-fn part2(rules: &Rules, tests: &Tests) -> u32 {
-    // fix bad tests then count
-    let mut tally = 0;
-    let keys_list: Vec<String> = rules.keys()
-        .map(|k| k.to_owned())
-        .collect();
-    for fail in tests {
-        let mut revised = fail.clone();
-        let test_len = fail.len();
-        'outer: loop {
-            for i in (1..test_len).rev() {
-                let current_page = revised[i].clone();
-                if !keys_list.contains(&current_page) { continue };
-                let prev_pages = Vec::from(&revised[..i]);
-                let curr_p_list = rules[&current_page].clone();
-                for (index, page) in prev_pages.iter().enumerate() {
-                    if curr_p_list.contains(&page) {
-                        // rule violation
-                        revised[i] = page.to_owned();
-                        revised[index] = current_page.to_owned();
-                        continue 'outer
-                    }
-                }
-            }
-            // tally midpoint
-            let midpoint = test_len/2usize;
-            tally += revised[midpoint].clone().parse::<u32>().unwrap();
-            break 'outer
-        }
-    }
-    tally
+fn part2(test: u32) -> u32 {
+    todo!()
 }
 
 pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
