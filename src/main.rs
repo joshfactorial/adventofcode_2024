ceda8ded2233039af2bf094d5e056dec969a7b66
Joshua Allen <joshfactorial@gmail.com>
2024-12-13 20:02:31 -0600

    final for day 7, mostly copped from someone else and then adjusted

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,136 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::ops::{Add, Mul};
 use std::io::{BufRead, BufReader, Write};
 
-fn add_expr(lists: Vec<Vec<std::ops>>) -> Vec<Vec<std::ops>> {
-    let mut daughter_lists = Vec::new();
-    for i in 0..lists.len() {
-        let mut daughter = lists[i].clone();
-        daughter.push(usize::add);
-        daughter_lists.push(daughter.clone());
-        let mut daughter = lists[i].clone();
-        daughter.push(usize::mul);
-        daughter_lists.push(daughter.clone())
-    }
-    daughter_lists
+struct Equation {
+    lhs: u64,
+    rhs: Vec<u64>,
 }
 
-fn main() {
-    let file = read_lines("/home/joshfactorial/code/inputs/inputs.txt").unwrap();
-    let file_map = parse_lines(file);
-    for (key, vector_list) in file_map {
-        let num_expr = vector_list.len() - 1;
-        let mut expr_lists = Vec::with_capacity(2_usize.pow(num_expr as u32));
-        let expr_row = Vec::new();
-        expr_lists.push(expr_row);
-        let daughter_expr = add_expr(expr_lists);
+impl Equation {
+    fn is_valid(&self, extended_operator_set: bool) -> bool {
+        let last_element = self.rhs.last().unwrap();
+        if self.rhs.len() == 1 {
+            return if *last_element == self.lhs {
+                true
+            } else {
+                false
+            };
+        };
 
-        println!("daughter list: {:?}", daughter_expr)
+        if extended_operator_set {
+            let lhs_string = self.lhs.to_string();
+            let last_element_string = last_element.to_string();
+            let can_be_concatenated = lhs_string.ends_with(&last_element_string);
+            if can_be_concatenated {
+                let mut new_rhs = self.rhs.clone();
+                new_rhs.pop();
+
+                let new_equation = Self {
+                    lhs: lhs_string
+                        .strip_suffix(&last_element_string)
+                        .unwrap()
+                        .parse::<u64>()
+                        .unwrap_or(0),
+                    rhs: new_rhs,
+                };
+
+                if new_equation.is_valid(true) {
+                    return true
+                }
+            }
+        }
+        let is_lhs_divisible_by_last_element = self.lhs % last_element == 0;
+        if is_lhs_divisible_by_last_element {
+            let mut new_rhs = self.rhs.clone();
+            new_rhs.pop();
+
+            let new_equation = Self {
+                lhs: self.lhs / last_element,
+                rhs: new_rhs,
+            };
+            if new_equation.is_valid(extended_operator_set) {
+                return true;
+            }
+        }
+
+        let mut new_rhs = self.rhs.clone();
+        new_rhs.pop();
+
+        let new_equation = Self {
+            lhs: self.lhs.saturating_sub(*last_element),
+            rhs: new_rhs,
+        };
+
+        if new_equation.is_valid(extended_operator_set) {
+            return true;
+        }
+
+        false
     }
 }
-
-fn parse_map(file_map: HashMap<usize, Vec<usize>>) -> usize {
-    let v = 0;
-    for (key, value) in file_map {
-        if value.len() < 2 { continue }
-        let mut tally = 0;
-        let num_ops = value.len() - 1;
-    }
-    v
-}
-
-fn parse_lines(lines: io::Lines<BufReader<File>>) -> HashMap<usize, Vec<usize>> {
-    let mut value_map: HashMap<usize, Vec<usize>> = HashMap::new();
-    for line in lines {
-        let safe_line = line.unwrap().clone();
-        let k_v_split: Vec<&str> = safe_line.split(":").collect::<Vec<&str>>().clone();
-        let key = k_v_split[0].parse::<usize>().unwrap();
-        let value_list: Vec<usize> = k_v_split[1]
-            .split_whitespace()
-            .map(|v| v.parse::<usize>().unwrap())
-            .collect();
-        value_map.insert(key, value_list);
-    }
-    value_map
-}
-
 
 pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
     // This creates a buffer to read lines
     let file = File::open(filename)?;
     Ok(BufReader::new(file).lines())
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let equations = parse_input(input);
+
+    Some(
+        equations
+            .iter()
+            .filter_map(|equation| match equation.is_valid(false) {
+                true => Some(equation.lhs),
+                false => None,
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let equations = parse_input(input);
+
+    Some(
+        equations
+            .iter()
+            .filter_map(|equation| match equation.is_valid(true) {
+                true => Some(equation.lhs),
+                false => None,
+            })
+            .sum()
+    )
+}
+
+fn parse_input(filename: &str) -> Vec<Equation> {
+    let file = File::open(filename).unwrap();
+    let input = BufReader::new(file).lines();
+    input
+        .map(|l| {
+            let l_unwrapped = l.unwrap();
+            let mut parts = l_unwrapped.split(":");
+            let lhs = parts.next().unwrap().parse::<u64>().unwrap();
+            let rhs = parts
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(|v| v.parse::<u64>().unwrap())
+                .collect();
+
+            Equation { lhs, rhs }
+        })
+        .collect()
+}
+
+
+
+fn main() {
+    // let result = part_one("/home/joshfactorial/code/inputs/inputs.txt.full").unwrap();
+    let result = part_two("/home/joshfactorial/code/inputs/inputs.txt.full").unwrap();
+    println!("Result = {result}");
 }
 
 #[cfg(test)]
@@ -69,12 +139,8 @@ mod tests {
 
     #[test]
     fn test_setup() {
-        let file = read_lines("/home/joshfactorial/code/inputs/inputs.txt");
-        let input = parse_lines(file.unwrap());
+        let input = parse_input("/home/joshfactorial/code/inputs/inputs.txt");
         assert_eq!(Vec::from([10, 19]), input[&190]);
         assert_eq!(Vec::from([11, 6, 16, 20]), input[&292]);
     }
-    // ((9 + 7) + 18) + 13
-    // ((9 * 7) + 18) + 13
-    // ((9 + 7) * 18) + 13
 }
